8ca42b1d4e02d47eb93d63ba6f3f9ed154948162
Tyler N. Ford <tylernford@gmail.com>
2018-05-20 07:38:28 -0500

    Updated map styles to match Transwestern

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,7 @@ mix
 	.sass('resources/sass/styles.scss', 'static/assets/css/styles.css')
 	//.js('resources/assets/js/app.js', 'public/assets/js/app.js')
 	.copyDirectory('resources/fonts', 'static/assets/fonts')
+	.copyDirectory('resources/icons', 'static/assets/icons')
 	.copyDirectory('resources/img', 'static/assets/img')
 	.version()
 	.options({
